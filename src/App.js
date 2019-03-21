43f740799d6435c6a66c3f24b2de41c432b5d3a6
Jacques Le Breuilly <jacques.lebreuilly@sidigital.co>
2019-03-21 12:03:40 +0000

    Testing changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> testing text
+            EDIT <code>src/App.js</code> sting tddextdfgdfdddg
           </p>
           <a
             className="App-link"
