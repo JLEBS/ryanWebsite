109ae11453187f6f5f461d2ee061086bd7902ef1
Jacques Le Breuilly <jacques.lebreuilly@sidigital.co>
2019-03-21 14:03:29 +0000

    Initialization stage, installing other packages

---
+++ src/components/homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Wrapper from './wrapper';
+import Heading from './heading';
+
+const HomePage = () => (
+
+<Wrapper>
+    <Heading as='h2' primary >
+        Ryan Callard
+    </Heading> 
+    <Heading>
+        Ryan Callard
+    </Heading>
+</Wrapper>
+);
+
+export default HomePage;
---
+++ src/components/wrapper.js
@@ -0,0 +1,7 @@
+import styled, {css} from 'styled-components';
+
+const Wrapper = styled.div`
+    background-color: grey;
+`
+
+export default Wrapper;
