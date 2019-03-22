0bad46c939fbcb5788ac05653f2b5e9777656169
Jacques Le Breuilly <jacques.lebreuilly@sidigital.co>
2019-03-22 09:49:31 +0000

    Image development

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { SocialMediaGroup, SocialMediaItem } from './images';
 
+const themePath = 'Users/jacqueslebreuilly/ryanWebsite/src/img/';
+
+
 const Footer = () => (
   <footer>
     <SocialMediaGroup>
-      <SocialMediaItem imageUrl='/img/twitter.png' />
-      <SocialMediaItem imageUrl='/img/youtube.png' />
-      <SocialMediaItem imageUrl='/img/linkedin.png' />
+      <SocialMediaItem imageUrl='Users/jacqueslebreuilly/ryanWebsite/src/img/Twitter.svg' />
+      <SocialMediaItem imageUrl='../img/youtube.svg' />
+      <SocialMediaItem imageUrl='/src/img/linkedin.svg' />
     </SocialMediaGroup>
   </footer>
 );
